cc792e9194d69e253f891027b1934a533af8c6cf
WuRC <763484298@qq.com>
2020-05-16 20:23:36 +0800

    vue测试环境安装

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <input v-model="value" />
+    </div>    
+</template>
+
+<script>
+    export default {
+        name:"input",
+        data(){
+            return {
+                value:"123"
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
